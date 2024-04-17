5b74365f5b41b4b23410a43124dca4ccde4f7aa4
yllari <139646541+yllari@users.noreply.github.com>
2024-04-17 12:11:42 +0200

    Create ruwe.py

---
+++ RUWE/ruwe.py
@@ -0,0 +1,33 @@
+import numpy as np
+import vaex
+import matplotlib.pyplot as plt
+### Fast and less-than-ideal plotting
+def ruwe_separate(df):
+    return df[df['ruwe']<1.4], df[df['ruwe']>=1.4]
+
+df = vaex.open("../../6D_full_halo_sample_2_5kpc_no_quality_cuts_wAG.hdf5")
+df = df[np.abs(df['MG']) < 1e3]
+df = df[np.abs(df['COL_bp_rp']) < 1e3]
+df1, df2 = ruwe_separate(df)
+max_valMG = np.amax(df2['MG'].values)
+min_valMG = np.amin(df2['MG'].values)
+
+max_valCOL = np.amax(df2['COL_bp_rp'].values)
+min_valCOL = np.amin(df2['COL_bp_rp'].values)
+
+fig, ax = plt.subplots(1,2)
+fig.set_size_inches(8,5)
+ax[0].hist2d(df1['COL_bp_rp'].values, df1['MG'].values, bins=100)
+ax[0].set_xlim([min_valCOL, max_valCOL])
+ax[0].set_ylim([max_valMG, min_valMG])
+ax[0].set_xlabel("COL BP-RP")
+ax[0].set_ylabel(r"$M_G$")
+ax[0].set_title("RUWE<1.4")
+plt.close()
+ax[1].hist2d(df2['COL_bp_rp'].values, df2['MG'].values, bins=100)
+ax[1].set_xlim([min_valCOL, max_valCOL])
+ax[1].set_ylim([max_valMG, min_valMG])
+ax[1].set_xlabel("COL BP-RP")
+ax[1].set_title("RUWE>=1.4")
+fig.suptitle("All Halo dat")
+fig.savefig("plot_ruwe>14.png", dpi=300)
